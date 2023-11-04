81218a8a51c8e2337da7d59bca0cce2a0d2b5109
Yang Zhao <jenson.zhao@gmail.com>
2023-11-03 20:52:32 -0700

    config env vars

--- pipeline.sh
+++ pipeline.sh
@@ -13,20 +13,20 @@ dokku certs:add ${app_name} /home/dokku/server.crt /home/dokku/server.key
 dokku ports:set ${app_name} http:80:8080 https:443:8080
 
 git remote add dokku dokku@moochee.us:${app_name} || git remote set-url dokku dokku@moochee.us:${app_name}
-# dokku config:set --no-restart ${app_name} \
-#     ENV=$ENV \
-#     ADMIN_SECRET=$ADMIN_SECRET \
-#     DATABASE_TYPE=$DATABASE_TYPE \
-#     DATABASE_URL=$DATABASE_URL \
-#     PORT=$PORT \
-#     AUTHORIZER_URL=$AUTHORIZER_URL \
-#     DISABLE_BASIC_AUTHENTICATION=$DISABLE_BASIC_AUTHENTICATION \
-#     ROLES=$ROLES \
-#     DEFAULT_ROLES=$DEFAULT_ROLES \
-#     JWT_ROLE_CLAIM=$JWT_ROLE_CLAIM \
-#     COOKIE_NAME=$COOKIE_NAME \
-#     GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID \
-#     GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET \
+dokku config:set --no-restart ${app_name} \
+    ENV=$ENV \
+    ADMIN_SECRET=$ADMIN_SECRET \
+    DATABASE_TYPE=$DATABASE_TYPE \
+    DATABASE_URL=$DATABASE_URL \
+    PORT=$PORT \
+    AUTHORIZER_URL=$AUTHORIZER_URL \
+    DISABLE_BASIC_AUTHENTICATION=$DISABLE_BASIC_AUTHENTICATION \
+    ROLES=$ROLES \
+    DEFAULT_ROLES=$DEFAULT_ROLES \
+    JWT_ROLE_CLAIM=$JWT_ROLE_CLAIM \
+    COOKIE_NAME=$COOKIE_NAME \
+    GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID \
+    GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET
 # git push dokku main
 
 # echo "Maping public route"
