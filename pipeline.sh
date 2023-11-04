8f4626538e557e52cb07d7bc822efc7f481cc087
Yang Zhao <jenson.zhao@gmail.com>
2023-11-03 20:40:18 -0700

    use ports:set

--- pipeline.sh
+++ pipeline.sh
@@ -10,7 +10,7 @@ fi
 dokku apps:create ${app_name}
 dokku storage:mount ${app_name} /var/lib/dokku/data/storage/moochee/auth:/auth
 dokku certs:add ${app_name} /home/dokku/server.crt /home/dokku/server.key
-dokku proxy:ports-set ${app_name} http:80:8080 https:443:8080
+dokku ports:set ${app_name} http:80:8080 https:443:8080
 
 git remote add dokku dokku@moochee.us:${app_name} || git remote set-url dokku dokku@moochee.us:${app_name}
 dokku config:set --no-restart ${app_name} \
